bc7e348c46d1db8e85e7f0aa88811b058414c1e4
Harold Stiven Vargas Henao <115584917+Har-day@users.noreply.github.com>
2022-11-10 17:43:06 -0500

    Add files via upload

---
+++ Quiz/QuizE1.py
@@ -0,0 +1,15 @@
+'''
+Created on 10/11/2022
+
+@author: Harday
+'''
+Rivers={'Amazonas':'Colombia,Peru y Brasil','Nilo':'Uganda, Sudán y Egipto','Misisispi':'Estados Unidos','Yangtsé':'China','Volga':'Rusia'}
+for i in Rivers:
+    print('El rio '+i+ ' corre a traves de el/los pais/es: '   +Rivers[i])
+    print('\n')
+print('----------------------------------------------------------------')
+for i in Rivers:
+    print(i+'\n')
+print('----------------------------------------------------------------')
+for i in Rivers:
+    print(Rivers[i]+'\n')
---
+++ Quiz/QuizE2.py
@@ -0,0 +1,9 @@
+'''
+Created on 10/11/2022
+
+@author: Harday
+'''
+Glosario = {'Python':' Es un lenguaje de alto nivel de programación interpretado cuya filosofía hace hincapié en la legibilidad de su código, se utiliza para desarrollar aplicaciones de todo tipo, ejemplos: Instagram, Netflix, Spotify, Panda3D, entre otros.​','Tupla':' Es una lista ordenada finita de elementos. Una n-tupla Es una secuencia de n elementos, donde n Es un número entero no negativo. Sólo hay una tupla 0, denominada tupla vacía. Una n-tupla se define inductivamente mediante la construcción de un par ordenado.', 'Iteracion':' Significa repetir varias veces un proceso con la intención de alcanzar una meta deseada, objetivo o resultado ​. Cada repetición del proceso también se le denomina una "iteración", y los resultados de una iteración se utilizan como punto de partida para la siguiente iteración.','Paradigma':' Es utilizado comúnmente como sinónimo de “ejemplo” o para hacer referencia en caso de algo que se toma como “modelo". En principio se tenía en cuenta en el campo, tema, ámbito, entre dos personalidades u otros..., gramatical y se valoraba desde la retórica.','GitHub':' Es una forja para alojar proyectos utilizando el sistema de control de versiones Git. Se utiliza principalmente para la creación de código fuente de programas de ordenador. El software que opera GitHub fue escrito en Ruby on Rails. Desde enero de 2010, GitHub opera bajo el nombre de GitHub, Inc'}
+for i in Glosario:
+    print(i+ ':'   +Glosario[i])
+    print('\n')
